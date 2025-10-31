ed44c74b0ff99ed3bcaf812ccd18ffe9c64db812
Thomas Nabord <thomas.nabord@prestashop.com>
2025-10-31 11:30:04 +0000

    Bump to v2.1.0

--- statsbestvouchers.php
+++ statsbestvouchers.php
@@ -41,7 +41,7 @@ class statsbestvouchers extends ModuleGrid
     {
         $this->name = 'statsbestvouchers';
         $this->tab = 'administration';
-        $this->version = '2.0.1';
+        $this->version = '2.1.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
